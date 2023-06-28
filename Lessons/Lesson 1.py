08d98fd62566e21a725ccc5ea62d7dd994d1081b
jalogavi92 <86204906+jalogavi92@users.noreply.github.com>
2023-06-28 16:56:38 -0500

    First Homework

---
+++ Lessons/Lesson 1.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+favorite_food = input("Enter your favorite food: ")
+
+print("Hello " + name + " ! " + " You are " + age + " years old and your favorite food is " + favorite_food )
+
