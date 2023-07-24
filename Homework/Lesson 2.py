f8d675079fb9fe18e8e90038ead4eb753233a22a
jalogavi92 <86204906+jalogavi92@users.noreply.github.com>
2023-07-23 22:35:32 -0500

    5

--- Homework/Lesson 2.py
+++ Homework/Lesson 2.py
@@ -1,7 +1,9 @@
 import random
 
+
 def generate_random_list(length, min_val, max_val):
     return [random.randint(min_val, max_val) for _ in range(length)]
+
 
 def find_max_min_avg(numbers):
     if not numbers:
@@ -10,6 +12,7 @@ def find_max_min_avg(numbers):
     minimum = min(numbers)
     average = sum(numbers) / len(numbers)
     return maximum, minimum, average
+
 
 length = 10
 min_val = 1
